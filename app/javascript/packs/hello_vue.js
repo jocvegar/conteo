bdaaa551609887f72e1e4253dce68889b2d19ce9
Jose <jocvegar@gmail.com>
2020-08-27 19:53:40 -0600

    cleans up code

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -3,14 +3,14 @@ import TurbolinksAdapter from 'vue-turbolinks'
 import Vue from 'vue/dist/vue.esm.js';
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import App from '../components/app.vue'
-import Test from '../components/test.vue'
+import voteForm from '../components/vote_form.vue'
 import NavBar from '../components/navbar.vue'
 
 Vue.use(TurbolinksAdapter)
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 Vue.component('app', App)
-Vue.component('test', Test)
+Vue.component('vote_form', voteForm)
 Vue.component('navbar', NavBar)
 
 document.addEventListener('turbolinks:load', () => {
