e246f72be12ff9bd3affaa16f8d289e2ba11c6a4
Jose <jocvegar@gmail.com>
2020-08-27 18:30:58 -0600

    Adds redis

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,7 @@ Zone.create([{ name: 'Tegucigalpa' },{ name: 'Comayagüela' },{ name: 'Las Casit
 City.create!([{ name: 'Ciudad 1', zone: Zone.first },{ name: 'Ciudad 2', zone: Zone.second },{ name: 'Ciudad 3', zone: Zone.last }])
 
 VotingCenter.create!([{ name: 'Centro de Votación 1', city: City.first },{ name: 'Centro de Votación 2', city: City.second },{ name: 'Centro de Votación 3', city: City.last }])
+
+tally = User.create!(email: "registro@example.com", password: "password", role: "tally")
+
+tally.build_user_location(voting_center_id: VotingCenter.first)
